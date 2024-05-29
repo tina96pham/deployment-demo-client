cad3aad26342646e0971a24f5fe970b5965914c6
tina96pham <tina96pham@gmail.com>
2024-05-29 11:29:57 -0400

    first commit

---
+++ src/pages/Warehouse.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import DonutSpinner from "../components/DonutSpinner";
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+const Warehouse = () => {
+  const [warehouses, setWarehouses] = useState([]);
+
+  useEffect(() => {
+    const fetchWarehouses = async () => {
+      try {
+        const response = await axios.get(`${API_URL}/warehouses`);
+        setWarehouses(response.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchWarehouses();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Warehouses</h1>
+      {!warehouses.length ? (
+        <DonutSpinner />
+      ) : (
+        <ul>
+          {warehouses.map((warehouse) => (
+            <li key={warehouse.id}>{warehouse.name}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Warehouse;
